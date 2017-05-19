e9449fbd592674e5f447da65d3586239fea057d0
wrsx <mewithmyclub@hotmail.com>
2017-05-20 00:19:45 +0800

    Implement templating for index & signup page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'StudyFinder - Home' , name: 'index', loggedIn: false});
+});
+
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'StudyFinder - Signup' , name: 'signup', loggedIn: false});
 });
 
 module.exports = router;
