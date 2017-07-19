9e969ceadd35449f517c6b24971bf35b1ce3c567
Jim Reynolds <jim4hire@gmail.com>
2017-07-20 00:26:59 +0800

    new database

--- routes/db.js
+++ routes/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://erklik:tusif556571@ds147681.mlab.com:47681/cits3403mac'
+var dbURI = 'mongodb://jimorey:jim123@ds117271.mlab.com:17271/studyfinderdb'
 //var dbURI = 'mongodb://localhost:27017/CITS3403'
 mongoose.connect(dbURI);
 
